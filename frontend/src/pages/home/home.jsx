f501b49d822a5affcd21c9e254ea6b4695222d37
ChinmayPatil07 <cypatil0711@gmail.com>
2024-10-01 19:39:14 +0530

    Commit Home page

---
+++ frontend/src/pages/home/home.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from 'react'
+import './home.css'
+import Header from '../../components/header/header'
+import ExploreMenu from '../../components/exploreMenu/exploreMenu'
+import FoodDisplay from '../../components/foodDisplay/foodDisplay'
+
+const Home = () => {
+
+    const [category, setCategory] = useState("All");
+
+  return (
+    <div>
+      <Header/>
+      <ExploreMenu category={category} setCategory={setCategory}/>
+      <FoodDisplay category={category}/>
+    </div>
+  )
+}
+
+export default Home
